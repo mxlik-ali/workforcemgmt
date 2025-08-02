d2b5d97c415071a145fa1d4f9295f44a77c0866c
mxlik-ali <alimalik23142@gmail.com>
2025-08-03 03:04:53 +0530

    Initial commit - pushing to existing GitHub repo

---
+++ src/main/java/com/railse/hiring/workforcemgmt/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.railse.hiring.workforcemgmt.common.exception;
+
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
+
+
+
+
