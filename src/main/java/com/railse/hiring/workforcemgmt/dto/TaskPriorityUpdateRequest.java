3d9d33043f8f38d86713573eb6ddf956681383af
mxlik-ali <alimalik23142@gmail.com>
2025-08-03 16:49:23 +0530

    fix: added a Interface method updateTaskPriority

--- src/main/java/com/railse/hiring/workforcemgmt/dto/TaskPriorityUpdateRequest.java
+++ src/main/java/com/railse/hiring/workforcemgmt/dto/TaskPriorityUpdateRequest.java
@@ -1,2 +1,13 @@
-package com.railse.hiring.workforcemgmt.dto;public class TaskPriorityUpdateRequest {
+package com.railse.hiring.workforcemgmt.dto;
+
+import com.railse.hiring.workforcemgmt.model.enums.Priority;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+@Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class TaskPriorityUpdateRequest {
+    private Long taskId;
+    private Priority priority;
 }
